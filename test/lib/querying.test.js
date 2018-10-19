8ccc7a09c69e330aab416009a0d134f00f37c1f1
Thomas Marcon <thomas.marcon@chauffeur-prive.com>
2018-10-19 16:20:28 +0200

    rename and split test files

--- test/lib/querying.test.js
+++ test/lib/querying.test.js
@@ -6,7 +6,7 @@ const mysql = require('mysql2/promise');
 
 const MysqlConnector = require('../../lib/MysqlConnector');
 
-describe('Querying', () => {
+describe('Querying database', () => {
   let mysqlConnection;
   const sandbox = sinon.createSandbox();
   const mysqlConfig = {
@@ -119,107 +119,6 @@ describe('Querying', () => {
       expect(last).to.equal(
         'SELECT ? AS test1, ? AS test2 FROM DUAL [joe, mocha]',
       );
-    });
-  });
-
-  describe('#findMany', () => {
-    const mysqlConector = new MysqlConnector(mysqlConfig);
-
-    beforeEach(async () => {
-      await mysqlConector.connect();
-    });
-
-    afterEach(async () => {
-      await mysqlConector.disconnect();
-    });
-
-    it('finds no matching rows in table', async () => {
-      const rows = await mysqlConector.findMany('tests', { type: 0 });
-      expect(rows).to.deep.equal([]);
-    });
-
-    it('finds all rows in table', async () => {
-      const rows = await mysqlConector.findMany('tests');
-      expect(rows).to.deep.equal([
-        { id: 1, name: 'test 1', type: 1 },
-        { id: 2, name: 'test 2', type: 3 },
-        {
-          id: 3,
-          name: 'test 3',
-          type: 3,
-        },
-      ]);
-    });
-
-    it('handles limit and offset parameter', async () => {
-      const rows = await mysqlConector.findMany(
-        'tests',
-        {},
-        { limit: 1, offset: 1 },
-      );
-      expect(rows).to.deep.equal([{ id: 2, name: 'test 2', type: 3 }]);
-    });
-
-    it('returns only required fields from matching rows in table', async () => {
-      const rows = await mysqlConector.findMany(
-        'tests',
-        {},
-        { projections: ['name'] },
-      );
-      expect(rows).to.deep.equal([
-        { name: 'test 1' },
-        { name: 'test 2' },
-        { name: 'test 3' },
-      ]);
-    });
-
-    it('returns all matching rows with a sort applied', async () => {
-      const rows = await mysqlConector.findMany(
-        'tests',
-        {},
-        { sort: { type: 'DESC', id: 'ASC' } },
-      );
-      expect(rows).to.deep.equal([
-        { id: 2, name: 'test 2', type: 3 },
-        { id: 3, name: 'test 3', type: 3 },
-        { id: 1, name: 'test 1', type: 1 },
-      ]);
-    });
-  });
-
-  describe('#findOne', () => {
-    const mysqlConector = new MysqlConnector(mysqlConfig);
-
-    beforeEach(async () => {
-      await mysqlConector.connect();
-    });
-
-    afterEach(async () => {
-      await mysqlConector.disconnect();
-    });
-
-    it('finds no matching row', async () => {
-      const row = await mysqlConector.findOne('tests', { id: 4 });
-      expect(row).to.deep.equal(null);
-    });
-
-    it('finds one row', async () => {
-      const row = await mysqlConector.findOne('tests');
-      expect(row).to.deep.equal({ id: 1, name: 'test 1', type: 1 });
-    });
-
-    it('finds one matching row with projections', async () => {
-      const row = await mysqlConector.findOne(
-        'tests',
-        { id: 3 },
-        { projections: ['name'] },
-      );
-      expect(row).to.deep.equal({ name: 'test 3' });
-    });
-
-    it('finds one matching row if more than one row matches', async () => {
-      const row = await mysqlConector.findOne('tests', { type: 3 });
-      expect(row).to.deep.equal({ id: 2, name: 'test 2', type: 3 });
     });
   });
 });
